0a7de3f8b741201c1f457f7cdad067dd9c2e2058
Rahul <rahulkappala699@gmail.com>
2023-07-12 14:40:19 -0500

    unit testing

--- SpringLearning/src/main/java/com/Spring/SpringLearning/StudentController.java
+++ SpringLearning/src/main/java/com/Spring/SpringLearning/StudentController.java
@@ -1,6 +1,5 @@
 package com.Spring.SpringLearning;
 
-import com.Spring.SpringLearning.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- SpringLearning/src/main/java/com/Spring/SpringLearning/service/StudentService.java
+++ SpringLearning/src/main/java/com/Spring/SpringLearning/service/StudentService.java
@@ -1,6 +1,5 @@
 package com.Spring.SpringLearning.service;
 
-import com.Spring.SpringLearning.model.Student;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
